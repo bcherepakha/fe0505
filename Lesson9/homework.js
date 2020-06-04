8ee2aebeeba752762a1de299951a4e3dcf0f77c2
Boris Cherepakha <bcherepakha@gmail.com>
2020-06-04 21:11:18 +0300

    Lesson 9 homework start

--- Lesson9/homework.js
+++ Lesson9/homework.js
@@ -10,6 +10,88 @@ console.log(ANCESTRY_DATA);
     }
 */
 
+function getFamily(human) {
+    const mother = getHuman(human.mother),
+        father = getHuman(human.father),
+        children = ANCESTRY_DATA.filter(function(child) {
+            return child.mother === human.name || child.father === human.name;
+        });
+
+    return {
+        human,
+        mother,
+        father,
+        children,
+        key: getFamilyKey(human)
+    }
+}
+
+function getUniqFamily() {
+    const data = ANCESTRY_DATA.map(getFamily),
+        keys = {
+            'null-null': true
+        };
+
+    return data.filter(function(family) {
+        const familyKey = family.key,
+            isUniq = !keys[familyKey];
+
+        keys[familyKey] = true;
+
+        return isUniq;
+    });
+}
+
+console.log( 'getFamily', ANCESTRY_DATA.map(getFamily) );
+console.log( 'getUniqFamily', getUniqFamily() );
+
+function getDataWithFamilyKey() {
+    return ANCESTRY_DATA.map(function(human) {
+        return Object.assign({}, human, {
+            familyKey: `${human.father}-${human.mother}`
+        });
+    });
+}
+
+function getFamilyKey(human) {
+    return `${human.father}-${human.mother}`;
+}
+
+function getFamilys() {
+    return ANCESTRY_DATA
+        .reduce(
+            function(families, human) {
+                const familyKey = getFamilyKey(human);
+
+                if (!Array.isArray(families[familyKey])) {
+                    families[familyKey] = [];
+                }
+
+                families[familyKey].push(human);
+
+                return families;
+            },
+            {}
+        );
+}
+
+console.log( 'getDataWithFamilyKey', getDataWithFamilyKey() );
+console.log( 'getFamilys', getFamilys() );
+console.log( 'getFamilysConverted',
+    Object.entries( getFamilys() )
+        .filter(function(familyData) {
+            const familyKey = familyData[0];
+
+            return familyKey !== 'null-null';
+        })
+);
+
+function getHuman(name) {
+    return ANCESTRY_DATA.find(function(human) {
+        return human.name === name;
+    });
+}
+
 function getAge(human) {
     return human.died - human.born;
 }
@@ -65,3 +147,21 @@ function task4() {
 }
 
 console.log('task4', task4() );
+
+// среднюю разницу между в возрасте между родителями
+function task2() {
+    const ageDiff = ANCESTRY_DATA.map(function(human) {
+        const mother = getHuman(human.mother),
+            father = getHuman(human.father);
+
+        if (mother && father) {
+            return Math.abs(getAge(mother) - getAge(father));
+        }
+
+        return null;
+    }).filter(function(ageDiff) { return ageDiff !== null; });
+
+    return getArrAverage(ageDiff);
+}
+
+console.log('task2', task2() );
